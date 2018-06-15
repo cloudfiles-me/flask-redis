9c937345e016b3403548b110550e841e2b7079f2
Ubuntu <ubuntu@ip-172-31-2-200.ec2.internal>
2018-06-15 01:15:48 +0000

    Implemented a List Data Type

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from rediscluster import StrictRedisCluster
 
 app = Flask(__name__)
-startup_nodes = [{"host": "cluster.clustercfg.use1.cache.amazonaws.com", "port": "6379"}]
+startup_nodes = [{"host": "cache-cluster.j130cz.clustercfg.use1.cache.amazonaws.com", "port": "6379"}]
 db = StrictRedisCluster(startup_nodes=startup_nodes, decode_responses=True, skip_full_coverage_check=True)
 
 @app.route('/')
@@ -13,10 +13,28 @@ def hello_world():
     else:
         return 'Key not ready'
 
-@app.route('/setname/<name>')
+@app.route('/listnames')
+def list():
+    nameslen = db.llen("names")
+    message = ''
+    if (nameslen > 0):
+        #for i in range(0, nameslen):
+        for i in reversed(range(0, nameslen)):
+            name = db.lindex('names', i)
+            message = message + "%s \n" %name
+        return message
+    else:
+        return 'No names in the system'
+
+@app.route('/addname/<name>')
 def setname(name):
-    db.set('Name', name.encode('utf-8'))
-    return 'Name updated'
+    db.lpush("names", name.encode('utf-8'))
+    return 'Name added'
+
+@app.route('/delname')
+def delname():
+    db.delete('Name')
+    return 'Name deleted'
 
 if __name__ == '__main__':
   app.run()
